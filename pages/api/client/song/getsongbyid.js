b9f41f4cd820bf037a34c01235ba319460954366
homerobdian <homerobdian@gmail.com>
2022-11-25 18:00:39 +0700

    1.1.7

--- pages/api/client/song/getsongbyid.js
+++ pages/api/client/song/getsongbyid.js
@@ -2,14 +2,22 @@ import database from "../../../../database/database.js";
 import Song from "../../../../models/SongModel.js";
 
 export default async (req, res) => {
+  const { songid, api_key } = req.query;
+  if (!songid) {
+    res.status(204).json({ msg: "empty Id" });
+  }
+  if (api_key != process.env.API_KEY) {
+    res.status(403).json({ msg: "invalid key" });
+  }
   database();
   try {
-    const data = await Song.findById(req.query.songid);
+    const data = await Song.findById(songid);
     if (!data) {
       res.status(404).json({ msg: "data not found" });
     }
     res.status(200).json(data);
   } catch (error) {
+    console.log(error);
     res.status(404).json({ msg: "failed fetch data" });
   }
 };
